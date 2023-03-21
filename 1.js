a5e48ec2a1bbd701908eb8575de6947b32d3ef9b
sarang_daddy <sarang_daddy@naver.com>
2023-03-21 23:36:54 +0900

    고차함수 사용해보기

--- 1.js
+++ 1.js
@@ -4,10 +4,10 @@ function solution(progresses, speeds) {
   // 각 작업은 배포까지 몇일이 걸리는가?
   const endCountArray = [];
 
-  for (let i = 0; i < progresses.length; i++) {
-    const endCount = Math.ceil((100 - progresses[i]) / speeds[i]);
+  progresses.forEach((workload, i) => {
+    const endCount = Math.ceil((100 - workload) / speeds[i]);
     endCountArray.push(endCount);
-  }
+  });
 
   // 첫번째 작업이 0이 되는 값만큼 모든 작업에 - 해준다.
   while (endCountArray.length > 0) {
@@ -32,16 +32,3 @@ function solution(progresses, speeds) {
 }
 
 console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
-
-// 남은 일수만으로 생각해서 오답 발생
-// while (endCountArray.length > 0) {
-//     let i = 0;
-//     let releaseCount = 0;
-//     while (endCountArray[i] >= endCountArray[i + 1]) {
-//       releaseCount++;
-//       endCountArray.shift();
-//     }
-//     releaseCount++;
-//     answer.push(releaseCount);
-//     endCountArray.shift();
-//   }
