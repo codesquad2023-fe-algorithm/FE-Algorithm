e50791ae22efff5afb58a3a9f69ecd8cd1344e9d
안예림 <an-yelim@an-yelim-ui-MacBookAir.local>
2023-03-22 00:11:30 +0900

    feat: 화요일 알고리즘 완료, 월요일 알고리즘 미완료

---
+++ [워터]w1/[PGS]기능개발.js
@@ -0,0 +1,18 @@
+const solution = (progresses, speeds) => {
+  const days = progresses.map((x, i) => Math.floor((100 - x) / speeds[i]));
+  let max = days[0];
+  let count = 1;
+  const result = [];
+  for (let i = 1; i < days.length; i++) {
+    if (max > days[i]) {
+      count += 1;
+    } else {
+      result.push(count);
+      count = 1;
+    }
+  }
+  return result;
+};
+
+console.log(solution([93, 30, 55], [1, 30, 5]));
+console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
