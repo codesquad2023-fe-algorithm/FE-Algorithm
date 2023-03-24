49b2e81e0720ca84ee10f03b8b880c2ba456817e
zlx454545 <zlx454545@gmail.com>
2023-03-24 22:29:52 +0900

    [PGS] 같은 숫자는 싫어 / Lv.1
    https://school.programmers.co.kr/learn/courses/30/lessons/12906

---
+++ [시저]w1/[PGS]같은숫자는싫어.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+  const answer = [];
+
+  answer.push(arr[0]);
+
+  for (let i = 1; i < arr.length; i++) {
+    if (answer[answer.length - 1] === arr[i]) continue;
+    answer.push(arr[i]);
+  }
+
+  return answer;
+}
