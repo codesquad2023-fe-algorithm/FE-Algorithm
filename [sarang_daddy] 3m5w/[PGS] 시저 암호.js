d13da39bc7673667335a2e6cb0c40d5599f4e14f
sarang_daddy <sarang_daddy@naver.com>
2023-04-01 22:59:50 +0900

    중복코드 제거

--- [sarang_daddy] 3m5w/[PGS] 시저 암호.js
+++ [sarang_daddy] 3m5w/[PGS] 시저 암호.js
@@ -6,26 +6,27 @@ function solution(s, n) {
   let answer = "";
 
   for (let i = 0; i < s.length; i++) {
-    let ascCode = s[i].charCodeAt();
-    console.log(ascCode);
+    const ascCode = s[i].charCodeAt();
+    let changedAscCode = ascCode + n;
 
-    if (ascCode === 32) answer += s[i];
-
-    if (97 <= ascCode && ascCode <= 122) {
-      let changedAscCode = ascCode + n;
-      if (changedAscCode > 122) {
-        changedAscCode -= 26;
-        answer += String.fromCharCode(changedAscCode);
-      } else answer += String.fromCharCode(changedAscCode);
+    if (ascCode === 32) {
+      answer += s[i];
+      continue;
     }
 
     if (65 <= ascCode && ascCode <= 90) {
-      let changedAscCode = ascCode + n;
       if (changedAscCode > 90) {
         changedAscCode -= 26;
-        answer += String.fromCharCode(changedAscCode);
-      } else answer += String.fromCharCode(changedAscCode);
+      }
     }
+
+    if (97 <= ascCode && ascCode <= 122) {
+      if (changedAscCode > 122) {
+        changedAscCode -= 26;
+      }
+    }
+
+    answer += String.fromCharCode(changedAscCode);
   }
 
   return answer;
