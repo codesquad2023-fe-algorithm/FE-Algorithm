082ae5b5277362db1cf6ed315e9e7b2bb25153b5
sarang_daddy <sarang_daddy@naver.com>
2023-04-05 22:42:58 +0900

    [PGS] 예산/Lv.1
    - https://school.programmers.co.kr/learn/courses/30/lessons/12982

---
+++ [sarang_daddy] 4m1w/[PGS] 예산.js
@@ -0,0 +1,16 @@
+function solution(d, budget) {
+  let answer = 0;
+  let sum = 0;
+  const sort = d.sort((a, b) => a - b);
+
+  for (let i = 0; i < sort.length; i++) {
+    if (sum + sort[i] <= budget) {
+      sum += sort[i];
+      answer++;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 3, 2, 5, 4], 9));
